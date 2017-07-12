2b8faf9c92a36cd97122d2e246f5222b74243255
Muzzh <murdyn@gmail.com>
2017-07-12 18:56:52 +0000

    set up the main frame

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/research'
+  get 'home/teaching'
+  get 'home/cv'
+  get 'home/art'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
