4ba60fb469fc442defa61fad11c2c9a5193c6f3a
Muzzh <murdyn@gmail.com>
2017-09-06 18:52:32 +0000

    added CV download, text for Art and changed home message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,12 @@ class HomeController < ApplicationController
   
   def art
   end
+
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/KeiserCV.pdf",
+      filename: "KeiserCV.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/teaching'
   get 'home/cv'
   get 'home/art'
+  get 'home/download_pdf'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
